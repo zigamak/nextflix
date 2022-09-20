8ab4c4c2c6027914a54b11a988b75c2b902260c9
zigamak <zigamakinde@gmail.com>
2022-09-20 23:18:24 +0100

    updated the design for the list and icons

--- src/components/ListItem/index.jsx
+++ src/components/ListItem/index.jsx
@@ -1,17 +1,27 @@
+import {useState} from 'react'
 import "./ListItem.scss"
 import {PlayArrow, ThumbDownAltOutlined, ThumbUpAltOutlined, Add} from '@material-ui/icons'
 
-const ListItem = () => {
+const ListItem = ({index}) => {
+  const [isHovered,setIsHovered] = useState(false);
+  const trailer="https://www.youtube.com/watch?v=FUK2kdPsBws&ab_channel=UniversalPicturesUK"
   return (
-    <div className="listItem">
+    <div 
+    className="listItem"
+    style={{left:isHovered && index * 225-50 + index * 2.5}}
+    onMouseEnter={()=>setIsHovered(true)} 
+    onMouseLeave={()=>setIsHovered(false)}
+    >
       <img src="https://stat1.bollywoodhungama.in/wp-content/uploads/2020/01/Fast-And-Furious-9-The-Fast-Saga-English.jpg" alt="" />
-    
+    {isHovered && (
+      <>
+    <video src={trailer}  autoPlay={true} loop />
     <div className="itemInfo">
       <div className="icons">
-        <PlayArrow/>
-        <Add/>
-        <ThumbUpAltOutlined/>
-        <ThumbDownAltOutlined/>
+        <PlayArrow className="icon" />
+        <Add className="icon"/>
+        <ThumbUpAltOutlined className="icon"/>
+        <ThumbDownAltOutlined className="icon"/>
 
       </div>
       <div className="itemInfoTop">
@@ -24,8 +34,10 @@ const ListItem = () => {
       </div>
       <div className="genre">
         Action 
-      </div>
+      </div>      
     </div>
+    </>
+        )}
   </div>
   )
 }
