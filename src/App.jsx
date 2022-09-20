512fb1786f203e86b33be4ebf6790c9787ec00f5
zigamak <zigamakinde@gmail.com>
2022-09-20 23:44:48 +0100

    updated icons designs

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 import "./app.scss"
 import { GlobalStyle } from './GlobalStyle';
 import Home from "./pages/Home"
+import Watch from "./pages/Watch"
 
 const App = () => (
   <div>
-    <Home/>
+    <Watch/>
     <GlobalStyle />
   </div>
 );
--- src/components/ListItem/index.jsx
+++ src/components/ListItem/index.jsx
@@ -4,7 +4,8 @@ import {PlayArrow, ThumbDownAltOutlined, ThumbUpAltOutlined, Add} from '@materia
 
 const ListItem = ({index}) => {
   const [isHovered,setIsHovered] = useState(false);
-  const trailer="https://www.youtube.com/watch?v=FUK2kdPsBws&ab_channel=UniversalPicturesUK"
+  const trailer =
+  "https://player.vimeo.com/external/371433846.sd.mp4?s=236da2f3c0fd273d2c6d9a064f3ae35579b2bbdf&profile_id=139&oauth2_token_id=57447761";
   return (
     <div 
     className="listItem"
